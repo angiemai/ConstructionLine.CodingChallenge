0eec121a7ffc5f41eaa41701546b260beab438be
AW <aweiruk@hotmail.com>
2021-02-23 23:10:30 +0000

    Add project files.

---
+++ ConstructionLine.CodingChallenge.Tests/SearchEngineTests.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace ConstructionLine.CodingChallenge.Tests
+{
+    [TestFixture]
+    public class SearchEngineTests : SearchEngineTestsBase
+    {
+        List<Shirt> shirts = new List<Shirt>
+            {
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Black - Medium", Size.Medium, Color.Black),
+                new Shirt(Guid.NewGuid(), "Blue - Large", Size.Large, Color.Blue),
+            };
+
+
+        [Test]
+        public void Test()
+        {
+            var searchEngine = new SearchEngine(shirts);
+
+            var searchOptions = new SearchOptions
+            {
+                Colors = new List<Color> {Color.Red},
+                Sizes = new List<Size> {Size.Small}
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            AssertResults(results.Shirts, searchOptions);
+            AssertSizeCounts(shirts, searchOptions, results.SizeCounts);
+            AssertColorCounts(shirts, searchOptions, results.ColorCounts);
+        }
+
+
+        [Test]
+        public void Test_For_Single_Color_Only()
+        {
+         
+            var searchEngine = new SearchEngine(shirts);
+
+            var searchOptions = new SearchOptions
+            {
+                Colors = new List<Color> { Color.Blue },
+                Sizes = new List<Size>() 
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            AssertResults(results.Shirts, searchOptions);
+            AssertSizeCounts(shirts, searchOptions, results.SizeCounts);
+            AssertColorCounts(shirts, searchOptions, results.ColorCounts);
+        }
+
+
+        [Test]
+        public void Test_For_Single_Size_Only()
+        {
+            var searchEngine = new SearchEngine(shirts);
+
+            var searchOptions = new SearchOptions
+            {
+                Colors = new List<Color>(),
+                Sizes = new List<Size> {  Size.Large}
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            AssertResults(results.Shirts, searchOptions);
+            AssertSizeCounts(shirts, searchOptions, results.SizeCounts);
+            AssertColorCounts(shirts, searchOptions, results.ColorCounts);
+        }
+
+
+        [Test]
+        public void Test_For_Multiple_Colors_And_Multiple_Sizes()
+        {
+            var searchEngine = new SearchEngine(shirts);
+
+            var searchOptions = new SearchOptions
+            {
+                Colors = new List<Color> { Color.Blue, Color.Red },
+                Sizes = new List<Size> { Size.Medium, Size.Large }
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            AssertResults(results.Shirts, searchOptions);
+            AssertSizeCounts(shirts, searchOptions, results.SizeCounts);
+            AssertColorCounts(shirts, searchOptions, results.ColorCounts);
+        }
+
+
+
+        [Test]
+        public void Test_For_All_Colors_And_All_Sizes()
+        {
+            var searchEngine = new SearchEngine(shirts);
+
+            var searchOptions = new SearchOptions
+            {
+                Colors =  Color.All,  
+                Sizes = Size.All 
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            AssertResults(results.Shirts, searchOptions);
+            AssertSizeCounts(shirts, searchOptions, results.SizeCounts);
+            AssertColorCounts(shirts, searchOptions, results.ColorCounts);
+        }
+
+        [Test]
+        public void Test_For_No_Colors_And_No_Sizes()
+        {
+            var searchEngine = new SearchEngine(shirts);
+
+            var searchOptions = new SearchOptions
+            {
+                Colors = new List<Color>(),
+                Sizes = new List<Size>()
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            AssertResults(results.Shirts, searchOptions);
+            AssertSizeCounts(shirts, searchOptions, results.SizeCounts);
+            AssertColorCounts(shirts, searchOptions, results.ColorCounts);
+        }
+    }
+}
+
---
+++ ConstructionLine.CodingChallenge/SearchEngine.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace ConstructionLine.CodingChallenge
+{
+
+    public class SearchEngine
+    {
+        private readonly List<Shirt> _shirts;
+        
+        List<Tuple<Color, List<Guid>>> colorLists = new List<Tuple<Color, List<Guid>>>();
+        List<Tuple<Size, List<Guid>>> sizeLists = new List<Tuple<Size, List<Guid>>>();
+
+        public SearchEngine(List<Shirt> shirts)
+        {
+
+            // TODO: data preparation and initialisation of additional data structures to improve performance goes here.
+
+            foreach (var x in Color.All)
+            {
+                colorLists.Add(new Tuple<Color, List<Guid>>(x, new List<Guid>()));
+            }
+
+            foreach (var x in Size.All) 
+            {
+                sizeLists.Add(new Tuple<Size, List<Guid>>(x, new List<Guid>()));
+            }
+
+            foreach (var x in shirts)
+            {
+                colorLists.Where(item=>item.Item1 == x.Color).Single().Item2.Add(x.Id);
+                sizeLists.Where(item => item.Item1 == x.Size).Single().Item2.Add(x.Id);
+            }
+
+            _shirts = shirts;
+        }
+
+
+        public SearchResults Search(SearchOptions options)
+        {
+           var colorCounts = new List<ColorCount>();
+            foreach (var x in colorLists) 
+            {
+                var colorCount = new ColorCount();
+                colorCount.Color = x.Item1;
+                colorCount.Count = options.Colors.Contains(x.Item1) ? x.Item2.Count() : 0;
+                colorCounts.Add(colorCount);
+            }
+
+            var sizeCounts = new List<SizeCount>();
+            foreach (var x in sizeLists) 
+            {
+                var sizeCount = new SizeCount();
+                sizeCount.Size = x.Item1; 
+                sizeCount.Count = options.Sizes.Contains(x.Item1) ? x.Item2.Count() : 0;
+                sizeCounts.Add(sizeCount);
+            }
+
+            return new SearchResults
+            {
+                Shirts = _shirts,  
+                ColorCounts = colorCounts,
+                SizeCounts = sizeCounts
+            };
+        }
+    }
+}
